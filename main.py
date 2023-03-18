bb35b16b95d9317ba64b255780f023b9c7ae8de5
_teejaay <devtam@teejaay.me>
2023-03-18 22:11:47 +0600

    env.txt

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.environ.get('SECRET_KEY')
+model = os.environ.get('MODEL')
 server_port = int(os.environ.get('GRADIO_SERVER_PORT'))
+content = os.environ.get('CONTENT')
 
 openai.api_key = api_key
 
-messages = [{"role": "system", "content": "You are a senior software developer"}]
+messages = [{"role": "system", "content": content}]
 
 
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model=model,
         messages=messages
     )
     ChatGPT_reply = response["choices"][0]["message"]["content"]
@@ -23,6 +25,6 @@ def CustomChatGPT(user_input):
     return ChatGPT_reply
 
 
-demo = gradio.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Software Developer")
+demo = gradio.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Software Development")
 
 demo.launch(share=True, server_port=server_port)
